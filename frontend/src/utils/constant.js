b58cea0590cedb7b34d6ea61cc35d7482209b1bb
heyysid18 <158181879+heyysid18@users.noreply.github.com>
2025-11-19 11:16:08 +0530

    Update API endpoints to use production URLs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT=["http://localhost:8001/api/v1/user","https://get-placed-i1w0.onrender.com/api/v1/user"];
-export const JOB_API_END_POINT=["http://localhost:8001/api/v1/job","https://get-placed-i1w0.onrender.com/api/v1/job"];
-export const APPLICATION_API_END_POINT=["http://localhost:8001/api/v1/application","https://get-placed-i1w0.onrender.com/api/v1/application"];
-export const COMPANY_API_END_POINT=["http://localhost:8001/api/v1/company","https://get-placed-i1w0.onrender.com/api/v1/company"];
+export const USER_API_END_POINT="https://get-placed-i1w0.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://get-placed-i1w0.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://get-placed-i1w0.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://get-placed-i1w0.onrender.com/api/v1/company";
 
 // Helper functions to get file URLs from MongoDB
 export const getProfilePhotoUrl = (userId) => {
@@ -14,4 +14,5 @@ export const getResumeUrl = (userId) => {
 
 export const getCompanyLogoUrl = (companyId) => {
     return companyId ? `${COMPANY_API_END_POINT}/logo/${companyId}` : null;
+
 };
